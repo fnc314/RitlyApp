9a7c5b5840f0b788f3ffa43d03d3eb6d57b54732
fnc314 <fnc314@gmail.com>
2014-01-24 12:29:41 -0800

    First Commit

---
+++ ritly_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RitlyApp::Application.config.secret_key_base = 'bdae25084e41fb1f56c38ad538529c3e9d408d082bbed8fb78fea60780efa919f986e0f6078b81ca45ae9a879c1fd6d2d0adc32aa272e793bfa66516ae31d111'
