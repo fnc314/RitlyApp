19e3a16cbacb554092b0c81dc6307f16d64f508b
fnc314 <fnc314@gmail.com>
2014-01-24 16:54:05 -0800

    update

--- ritly_app/app/controllers/go_controller.rb
+++ ritly_app/app/controllers/go_controller.rb
@@ -1,2 +1,15 @@
 class GoController < ApplicationController
+
+	def index
+
+	end
+
+	def redirect
+
+	end
+
+	def preview
+
+	end
+
 end
--- ritly_app/app/controllers/links_controller.rb
+++ ritly_app/app/controllers/links_controller.rb
@@ -1,2 +1,29 @@
 class LinksController < ApplicationController
+
+	def index
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def show
+
+	end
+
+	private #model methods (maybe?)
+
+	def unique_code_checker(random_string)
+
+	end
+
+	def unique_url(url)
+
+	end
+
 end
